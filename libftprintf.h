f5c88c4cbcf47434aa9be7b3ba4a6678f1815b21
Timon Reis <timonreis@gmx.at>
2026-05-04 15:17:37 +0200

    norm error

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: tireis <tireis@student.42vienna.com>      #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/04/30 20:06:54 by tireis           #+#    #+#              */
-/*   Updated: 2026/05/04 14:18:55 by tireis          ###   ########.fr        */
+/*   Updated: 2026/05/04 15:17:21 by tireis          ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ int	ft_putstr_pf(char *str);
 int	ft_puthex_lower(unsigned int n);
 int	ft_puthex_upper(unsigned int n);
 int	ft_putptr_pf(unsigned long long ptr);
-int	ft_printf(const char* format,...);
+int	ft_printf(const char *format, ...);
 
 #endif
